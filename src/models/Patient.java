a96d69aff9bb43dd12530bbe07200d1ba7bf4ceb
Aiemond‬‏ <m.shiha1111@gmail.com>
2025-12-16 23:09:03 +0300

    Add Patient model class

---
+++ src/models/Patient.java
@@ -0,0 +1,39 @@
+package models;
+
+/**
+ * Patient Model Class
+ * يمثل بيانات المريض في النظام
+ */
+public class Patient {
+    private String id;
+    private String name;
+    private int age;
+    private String phone;
+    
+    public Patient(String id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+    
+    public String getId() {
+        return id;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public int getAge() {
+        return age;
+    }
+    
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    
+    @Override
+    public String toString() {
+        return name + " (Age: " + age + ", ID: " + id + ")";
+    }
+}
